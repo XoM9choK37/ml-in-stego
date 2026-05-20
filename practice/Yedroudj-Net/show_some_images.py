c02716e312e7995f1c447e3ae1178d05b64e5163
XoM9choK37 <sanya.sasha.kostrov@mail.ru>
2026-05-20 05:53:02 +0300

    Add files

--- practice/Yedroudj-Net/show_some_images.py
+++ practice/Yedroudj-Net/show_some_images.py
@@ -2,18 +2,18 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import os
 
-directory = 'D:/Documents/DATASETS/BOSSbase_1.01_bmp'
+directory = 'D:/Documents/DATASETS/BOSSbase_1.01_bmp/cover_images'
+number_of_images = 20
 
 images = []
 for filename in os.listdir(directory):
-    if filename.endswith(('.png', '.jpg', '.jpeg', '.pgm', '.bmp')) and len(images) < 10:
+    if filename.endswith(('.png', '.jpg', '.jpeg', '.pgm', '.bmp')) and len(images) < number_of_images:
         image = Image.open(os.path.join(directory, filename))
         images.append(image)
+        continue
+    break
 
-if len(images) < 10:
-    raise ValueError("В папке должно быть как минимум 10 изображений.")
-
-fig, axes = plt.subplots(2, 5, figsize=(15, 6))
+fig, axes = plt.subplots(number_of_images // 5, 5, figsize=(15, 6))
 
 for i, ax in enumerate(axes.flatten()):
     ax.imshow(images[i])
